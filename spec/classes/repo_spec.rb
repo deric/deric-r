713031b6d17aeeef90e541203250b20b9a3b781c
Tomas Barton <barton.tomas@gmail.com>
2014-03-30 13:35:44 +0200

    fixed 1.8.7 support

--- spec/classes/repo_spec.rb
+++ spec/classes/repo_spec.rb
@@ -11,7 +11,7 @@ describe 'r::repo' do
     }}
 
     it { should contain_apt__source('r-project').with(
-     'location' => "http://cran.r-project.org/bin/linux/#{operatingsystem.downcase}",
+     'location' => "http://cran.r-project.org/bin/linux/#{operatingsystem.downcase}"
     )}
 
     context "manage_repo => false" do
